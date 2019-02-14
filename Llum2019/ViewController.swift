d5972002e4d61367e8c65116592df032054f23c9
Andrés Pizá <Tovkal@gmail.com>
2019-02-14 16:26:19 +0100

    Create project

---
+++ Llum2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Llum2019
+//
+//  Created by Andrés Pizá Bückmann on 14/02/2019.
+//  Copyright © 2019 Andrés Pizá Bückmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
